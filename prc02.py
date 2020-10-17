6456c1d6972ab5210b7623a0976fdb761efafecb
yang <hello.git.fila3894@naver.com>
2020-10-17 22:34:29 +0900

    두번째 코딩테스트(1차원 배열 이용)

---
+++ prc02.py
@@ -0,0 +1,8 @@
+n=int(input()) # 출석 번호를 부른 횟수(사람 명수)
+m=list(input().split(' ')) #무작위로 부른 1~23 사이의 출석번호
+a=list(0 for i in range(23))
+
+for i in range(1,24):
+    a[i-1]=m.count(str(i))
+for j in range(23):
+    print(a[j],end=' ')
